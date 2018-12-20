2ee148faa9b64aedd6b784de9317a37a2df37cf2
mu <mumayank@gmail.com>
2018-12-20 17:53:54 +0530

    update

--- app/src/main/java/mumayank/com/airshare2/TransferActivity.kt
+++ app/src/main/java/mumayank/com/airshare2/TransferActivity.kt
@@ -1,7 +1,10 @@
 package mumayank.com.airshare2
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import com.google.android.gms.nearby.connection.ConnectionInfo
@@ -9,15 +12,43 @@ import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
 import com.google.android.gms.nearby.connection.Payload
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate
 import mumayank.com.airshare.AirShare
+import mumayank.com.airshare.Utils
 import mumayank.com.airshare2.events.OnConnectionAcceptedOrRejected
 import mumayank.com.airshare2.events.OnEndpointFound
 import mumayank.com.airshare2.events.OnEndpointLost
 import org.greenrobot.eventbus.EventBus
+import android.support.v4.util.SimpleArrayMap
+import java.io.File
+import java.nio.charset.StandardCharsets
+import mumayank.com.airpermissions.AirPermissions
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import android.app.NotificationChannel
+import android.database.Cursor
+import android.net.Uri
+import android.provider.OpenableColumns
+
 
 class TransferActivity : AppCompatActivity() {
 
     var airShare: AirShare? = null
     var connectedEndpoints = ArrayList<String>()
+    var currentlySelectedEndpoint = ""
+    private var airPermission: AirPermissions? = null
+
+    val READ_REQUEST_CODE = 42
+    lateinit var ENDPOINT_ID_EXTRA: String
+
+    private val incomingFilePayloads = SimpleArrayMap<Long, Payload>()
+    private val completedFilePayloads = SimpleArrayMap<Long, Payload>()
+    private val filePayloadFilenames = SimpleArrayMap<Long, String>()
+
+    val incomingPayloads = SimpleArrayMap<Long, NotificationCompat.Builder>()
+    val outgoingPayloads = SimpleArrayMap<Long, NotificationCompat.Builder>()
+    lateinit var notificationManager: NotificationManager
+    val CHANNEL_ID = "AirShare"
 
     private enum class Stack {
         Select,
@@ -32,7 +63,33 @@ class TransferActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
         supportActionBar?.hide()
-        switchToSelectionFragment()
+        ENDPOINT_ID_EXTRA = Utils.getServiceId(this) + ".EndpointId"
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Downloads"
+            val description = "This keeps you up-to-dated about your incoming or outgoing files"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+
+        airPermission = AirPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            object: AirPermissions.Callbacks {
+                override fun onSuccess() {
+                    switchToSelectionFragment()
+                }
+
+                override fun onFailure() {
+                    finish()
+                }
+            }
+        )
     }
 
     fun switchToSelectionFragment() {
@@ -78,6 +135,7 @@ class TransferActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         airShare?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        airPermission?.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun getCommonCallback(): AirShare.CommonCallback {
@@ -118,6 +176,7 @@ class TransferActivity : AppCompatActivity() {
 
             override fun onConnected(endpointId: String) {
                 Toast.makeText(this@TransferActivity, "connected!", Toast.LENGTH_SHORT).show()
+                currentlySelectedEndpoint = endpointId
                 connectedEndpoints.add(endpointId)
                 EventBus.getDefault().post(OnConnectionAcceptedOrRejected())
                 switchToTransferFragment()
@@ -129,13 +188,110 @@ class TransferActivity : AppCompatActivity() {
             }
 
             override fun onPayloadReceived(endpointId: String, payload: Payload) {
-                // todo
+                if (payload.type == Payload.Type.BYTES) {
+                    val payloadFileNameMessage = String((payload.asBytes() as ByteArray), StandardCharsets.UTF_8)
+                    val payloadId = addPayloadFileName(payloadFileNameMessage)
+                    processFilePayload(payloadId)
+                } else if (payload.type == Payload.Type.FILE) {
+                    incomingFilePayloads.put(payload.id, payload)
+                }
+
+                if (payload.type == Payload.Type.BYTES) {
+                    // do nothing
+                } else {
+                    val notification = buildNotification(payload, true)
+                    notificationManager.notify(payload.id.toInt(), notification.build())
+                    incomingPayloads.put(payload.id, notification)
+                }
             }
 
             override fun onPayloadTransferUpdate(endpointId: String, payloadTransferUpdate: PayloadTransferUpdate) {
-                // todo
-            }
 
+                // notification
+                val payloadId = payloadTransferUpdate.payloadId
+                var notification: NotificationCompat.Builder? = null
+                if (incomingPayloads.containsKey(payloadId)) {
+                    notification = incomingPayloads.get(payloadId)
+                    if (payloadTransferUpdate.status != PayloadTransferUpdate.Status.IN_PROGRESS) {
+                        incomingPayloads.remove(payloadId)
+                    }
+                } else if (outgoingPayloads.containsKey(payloadId)) {
+                    notification = outgoingPayloads.get(payloadId)
+                    if (payloadTransferUpdate.status != PayloadTransferUpdate.Status.IN_PROGRESS) {
+                        outgoingPayloads.remove(payloadId)
+                    }
+                }
+                if (notification == null) {
+                    // do nothing
+                } else {
+                    when (payloadTransferUpdate.status) {
+                        PayloadTransferUpdate.Status.IN_PROGRESS -> {
+                            val size = payloadTransferUpdate.totalBytes
+                            if (size == (-1).toLong()) {
+                                // This is a stream payload, so we don't need to update anything at this point.
+                            } else {
+                                val percentTransferred = (100.toFloat() * ( payloadTransferUpdate.bytesTransferred.toFloat() / payloadTransferUpdate.totalBytes.toFloat() )).toInt()
+                                if (percentTransferred == 100) {
+                                    notification
+                                        .setProgress(100, 100, false)
+                                        .setContentTitle("Transfer complete!")
+                                } else {
+                                    notification.setProgress(100, percentTransferred, false)
+                                }
+                            }
+                        }
+                        PayloadTransferUpdate.Status.SUCCESS -> {
+                            notification
+                                .setProgress(100, 100, false)
+                                .setContentTitle("Transfer complete!")
+                        }
+                        PayloadTransferUpdate.Status.FAILURE -> {
+                            notification
+                                .setProgress(0,0,false)
+                                .setContentTitle("Transfer failed")
+                        }
+                        PayloadTransferUpdate.Status.CANCELED -> {
+                            notification
+                                .setProgress(0,0,false)
+                                .setContentTitle("Transfer failed")
+                        }
+                        else -> {
+                            Log.e("airshare", "Unknown status of notification update")
+                        }
+                    }
+                    notificationManager.notify(payloadId.toInt(), notification.build())
+                }
+
+                // actual work
+                if (payloadTransferUpdate.status == PayloadTransferUpdate.Status.SUCCESS) {
+                    val payloadId = payloadTransferUpdate.payloadId
+                    val payload = incomingFilePayloads.remove(payloadId)
+                    completedFilePayloads.put(payloadId, payload)
+                    if (payload?.type == Payload.Type.FILE) {
+                        processFilePayload(payloadId)
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun addPayloadFileName(payloadFileNameMessage: String): Long {
+        val parts = payloadFileNameMessage.split(":")
+        val payloadId = parts[0].toLong()
+        val fileName = parts[1]
+        filePayloadFilenames.put(payloadId, fileName)
+        return payloadId
+    }
+
+    private fun processFilePayload(payloadId: Long) {
+        val filePayload = completedFilePayloads.get(payloadId)
+        val fileName = filePayloadFilenames.get(payloadId)
+        if (filePayload != null && fileName != null) {
+            completedFilePayloads.remove(payloadId)
+            filePayloadFilenames.remove(payloadId)
+            val payloadFile = filePayload.asFile()?.asJavaFile()
+            payloadFile?.renameTo(File(payloadFile.parentFile, fileName))
         }
     }
 
@@ -159,4 +315,46 @@ class TransferActivity : AppCompatActivity() {
         airShare?.startDiscovery()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun buildNotification(payload: Payload, isIncoming: Boolean): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle( if (isIncoming) "Receiving..." else "Sending..." )
+            .setSmallIcon(R.drawable.ic_notification)
+            .setProgress(100, 0, false)
+            .setOnlyAlertOnce(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    }
+
+    interface Callbacks {
+        fun onSuccess(fileDisplayName: String, fileSizeInMB: Long)
+        fun onOperationFailed()
+    }
+
+    companion object {
+        fun extractFileProperties(activity: Activity, fileUri: Uri, callbacks: Callbacks) {
+            val cursor: Cursor? = activity.contentResolver.query(fileUri, null, null, null, null, null)
+            if (cursor == null) {
+                callbacks.onOperationFailed()
+            } else {
+                cursor.use {
+                    if (it.moveToFirst()) {
+                        val displayName: String = it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                        val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                        var size = 0L
+                        if (!it.isNull(sizeIndex)) {
+                            size = it.getLong(sizeIndex)
+                            size = (size/1024L)/1024L
+                        }
+                        callbacks.onSuccess(displayName, size)
+                    } else {
+                        callbacks.onOperationFailed()
+                    }
+                    cursor.close()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/mumayank/com/airshare2/TransferFragment.kt
+++ app/src/main/java/mumayank/com/airshare2/TransferFragment.kt
@@ -1,10 +1,24 @@
 package mumayank.com.airshare2
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.NotificationCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.Payload
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.lang.Exception
+import java.nio.charset.StandardCharsets
+
+
+
 
 class TransferFragment: Fragment() {
 
@@ -17,7 +31,76 @@ class TransferFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_transfer, container, false)
+
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "*/*"
+            startActivityForResult(intent, transferActivity.READ_REQUEST_CODE)
+        }
+
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == transferActivity.READ_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            view?.findViewById<ViewGroup>(R.id.progress)?.visibility = View.VISIBLE
+            view?.findViewById<View>(R.id.button)?.visibility = View.GONE
+
+            doAsync {
+                val endpointId = transferActivity.currentlySelectedEndpoint
+                val uri = data.data
+                val filePayload: Payload
+                try {
+                    val pfd = transferActivity.contentResolver.openFileDescriptor(uri, "r")
+                    filePayload = Payload.fromFile(pfd)
+                } catch (e: Exception) {
+                    uiThread {
+                        Toast.makeText(transferActivity, "Could not get file. Please try again.", Toast.LENGTH_SHORT).show()
+                        view?.findViewById<ViewGroup>(R.id.progress)?.visibility = View.GONE
+                        view?.findViewById<View>(R.id.button)?.visibility = View.VISIBLE
+                    }
+                    return@doAsync
+                }
+                TransferActivity.extractFileProperties(transferActivity, uri, object: TransferActivity.Callbacks {
+                    override fun onSuccess(fileDisplayName: String, fileSizeInMB: Long) {
+
+                        uiThread {
+                            val fileNameMessage = "${filePayload.id}:$fileDisplayName"
+                            val fileNameBytesPayload = Payload.fromBytes(fileNameMessage.toByteArray(StandardCharsets.UTF_8))
+                            Nearby.getConnectionsClient(transferActivity).sendPayload(endpointId, fileNameBytesPayload)
+                            Nearby.getConnectionsClient(transferActivity).sendPayload(endpointId, filePayload)
+
+                            startNotification(filePayload)
+                            view?.findViewById<ViewGroup>(R.id.progress)?.visibility = View.GONE
+                            view?.findViewById<View>(R.id.button)?.visibility = View.VISIBLE
+                        }
+                    }
+
+                    override fun onOperationFailed() {
+                        uiThread {
+                            Toast.makeText(transferActivity, "Could not get file. Please try again.", Toast.LENGTH_SHORT).show()
+                            view?.findViewById<ViewGroup>(R.id.progress)?.visibility = View.GONE
+                            view?.findViewById<View>(R.id.button)?.visibility = View.VISIBLE
+                        }
+                    }
+
+                })
+            }
+
+        }
+    }
+
+    fun startNotification(payload: Payload) {
+        if (payload.type == Payload.Type.BYTES) {
+            // return
+        }
+
+        val notification = transferActivity.buildNotification(payload, false)
+        transferActivity.notificationManager.notify(payload.id.toInt(), notification.build())
+        transferActivity.outgoingPayloads.put(payload.id, notification)
     }
 
 }
